0526656d5fe2aeb31ca9b6bb48d43d9d8b5adbf4
kawniy <fata.kawniy@gmail.com>
2022-06-02 23:19:52 +0700

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  * PHP Framework for Small Project
+
+  * @author Fata kawniy <fata.kawniy@gmail.com>
+  * @link https://github.com/kawniy/klepon
+*/
+
+require_once "../app/init.php";
+require_once "../vendor/autoload.php";
+$app = new App();
