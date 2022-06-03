902099611482afc2d48dbc846097903c11a6f107
kawniy <fata.kawniy@gmail.com>
2022-06-03 23:58:24 +0700

    initial commit

---
+++ app/Views/Error/404.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/css/style.css">
+  <title>404 Error Not Found</title>
+</head>
+<body>
+  <h1>404 Error Not Found</h1>
+</body>
+</html>
---
+++ app/Views/home.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Selamat Datang!!</title>
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+  <h1>Selamat Datang!!</h1>
+</body>
+</html>
