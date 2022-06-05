769047fef733dcb90570e93f60e3237996154116
kawniy <fata.kawniy@gmail.com>
2022-06-06 00:40:45 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  * PHP Framework for Small Project
+  * Green PHP Framework
 
   * @author Fata kawniy <fata.kawniy@gmail.com>
   * @link https://github.com/kawniy/klepon
@@ -8,4 +8,4 @@
 
 require_once "../app/init.php";
 require_once "../vendor/autoload.php";
-$app = new App();
+$app = new Route();
